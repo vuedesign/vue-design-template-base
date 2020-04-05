e6db267c8959ff1172193c81bf741e3d8fcbb620
n.see <gooarter@gmail.com>
2020-04-05 18:11:46 +0800

    update vue.configs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 const path = require('path');
-<<<<<<< HEAD
-=======
-
->>>>>>> 7bdf8f909e7cc44a313808e1cc66817329bc88bb
 function resolve (dir) {
     return path.join(__dirname, dir)
 }
@@ -10,27 +6,12 @@ function resolve (dir) {
 module.exports = {
     pages: {
         app: {
-<<<<<<< HEAD
           // page 的入口
           entry: 'src/main.js',
           // 模板来源
           template: 'index.html',
           // 在 dist/index.html 的输出
           filename: 'index.html'
-=======
-            // page 的入口
-            entry: 'src/main.js',
-            // 模板来源
-            template: 'public/index.html',
-            // 在 dist/index.html 的输出
-            filename: 'index.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'Index Page',
-            // 在这个页面中包含的块，默认情况下会包含
-            // 提取出来的通用 chunk 和 vendor chunk。
-            chunks: ['chunk-vendors', 'chunk-common', 'app']
->>>>>>> 7bdf8f909e7cc44a313808e1cc66817329bc88bb
         }
     },
     configureWebpack: {
